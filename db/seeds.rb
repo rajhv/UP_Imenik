b9f31f5cace5f351af0866015e9197744f27fcaf
Vojko Rajh <vojko.rajh@gmail.com>
2017-04-25 13:07:44 +0200

    seeds are created #3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+
+CSV.foreach("db/csv/Members.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Member.find_or_create_by(row.to_h)
+end
+
+CSV.foreach("db/csv/Departments.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Department.find_or_create_by(row.to_h)
+end
+
+CSV.foreach("db/csv/Employees.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Employee.find_or_create_by(row.to_h)
+end
