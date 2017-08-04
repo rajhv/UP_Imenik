98ff040c310aa424e5a74ff51fd72821add8999f
Vojko Rajh <vojko.rajh@gmail.com>
2017-08-04 12:39:54 +0200

    add login, sign-up, ...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,23 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Dodal po instalaciji Devise gem-a
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'devub', port: 3000 }
+  config.action_mailer.smtp_settings = {
+      user_name:      ENV['MAILER_EMAIL'],
+      password:       ENV['MAILER_PASSWORD'],
+      domain:         ENV['MAIL_HOST'],
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+
+
 
 
   config.action_mailer.perform_caching = false
